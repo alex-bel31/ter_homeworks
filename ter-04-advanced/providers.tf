aab9007a29cfaf9f064fcdeaf37a3355635b8d97
Vladimir <v4004462@gmail.com>
2025-05-17 12:16:05 +0000

    created backend s3

--- ter-04-advanced/providers.tf
+++ ter-04-advanced/providers.tf
@@ -1,9 +1,15 @@
 terraform {
+
+  backend "s3" {}
+
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "0.141.0"
     }
   }
+
+  required_version = ">=1.8.4"
 }
 
 provider "yandex" {
